29480a57d2e6de7e1f17f6081e25631803d0e3ed
MrsGuo <972134688@qq.com>
2019-08-10 23:55:08 +0800

    登录界面

---
+++ LoginView.js
@@ -0,0 +1,118 @@
+/**
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
+
+import React, { Component } from 'react';
+import {
+  View,
+  TextInput,
+  Text,
+  StyleSheet,
+  Image
+} from 'react-native'
+
+export default class MyApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./static/img/img.png')}
+          style={{
+            width: 80,
+            height: 80,
+            borderRadius: 40,
+            marginTop: 100,
+            marginBottom: 25
+          }}
+        />
+        <TextInput
+          placeholder="账号"
+          style={styles.loginInput}
+        />
+        <TextInput
+          placeholder="密码"
+          password={true}
+          style={styles.loginInput}
+        />
+        <View
+          style={styles.loginButton}
+
+        >
+          <Text>登录</Text>
+        </View>
+        <View
+          style={styles.loginSetting}
+        >
+          <Text>无法登陆?</Text>
+          <Text>忘记密码</Text>
+        </View>
+        <View style={styles.loginOther}>
+          <Text>其他登录方式:</Text>
+          <Image
+            source={require('./static/img/img.png')}
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 30,
+              marginLeft: 10
+            }} />
+          <Image
+            source={require('./static/img/img.png')}
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 30,
+              marginLeft: 10
+            }} />
+          <Image
+            source={require('./static/img/img.png')}
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 30,
+              marginLeft: 10
+            }} />
+        </View>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#dddddd',
+    alignItems: "center",
+  },
+  loginInput: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "white",
+    textAlign: "center",
+    marginTop: 1
+  },
+  loginButton: {
+    width: "90%",
+    height: 40,
+    backgroundColor: "#5687",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    borderRadius: 10
+  },
+  loginSetting: {
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  loginOther: {
+    flexDirection: "row",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 10,
+    left: 10,
+  }
+});
+
--- index.ios.js
+++ index.ios.js
@@ -7,53 +7,14 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput
 } from 'react-native';
-
+import App from './LoginView'
 export default class MyApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput style={styles.textInput}
-          // value="默认文字"
-          //调用键盘的类型，number-pad是数字键盘
-          keyboardType={"number-pad"}
-          //可以输入多行，密码框下设置会使密码框失效
-          // multiline={true}
-
-          //密码
-          // password={true}
-
-          //占位文字
-          // placeholder="我是占位文字"
-
-          //显示清除按钮
-          clearButtonMode={"always"}
-        />
-      </View>
+      <App />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  textInput: {
-
-    marginTop: 30,
-    width: 300,
-    height: 60,
-    //添加边框
-    borderWidth: 1,
-    borderColor: 'black'
-  }
-});
 
 AppRegistry.registerComponent('MyApp', () => MyApp);
